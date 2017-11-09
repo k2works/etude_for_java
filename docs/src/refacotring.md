3c1c07eb52c472a55d1adce1ea254966208b27c3
k2works <kakimomokuri@gmail.com>
2017-11-09 13:04:16 +0900

    第１章　リファクタリングー最初の例 #EFJ-5
    ・スタート地点

--- docs/src/refacotring.md
+++ docs/src/refacotring.md
@@ -1,6 +1,6 @@
 ---
 markdown:
-  image_dir: ../assets
+  image_dir: ./assets
   path: ../refactoring.md
   ignore_from_front_matter: true
   absolute_image_path: false
@@ -9,9 +9,62 @@ markdown:
 # リファクタリング
 
 ## 要求
+顧客が借りたビデオのレンタル料金を計算して計算書を印刷する。
+```puml
+@startuml
+left to right direction
+skinparam packageStyle rectangle
+actor customer
+rectangle VedioRental {
+  customer -- (Rental)
+              (Calculate rental fee)
+              (Print a statement)
+}
+@enduml
+```
 
 ## 仕様
++ ビデオレンタルの料金を計算して計算書を印刷するプログラム
++ システムにはどの映画を何日間借りるかが入力される。
++ 貸出の日数によって料金が計算され、映画の分類が判定される。
++ 映画の分類は３つある。一般向け、子供向け、新作。
++ レンタルポイントも印刷される。新作かどうかによってポイント計算の仕方が異なる。
 
 ## 設計
+### クラス図
+```puml
+class Movie {
+  priceCode:int
+}
+class Rental {
+  daysRented:int
+}
+class Customer {
+  statement()
+}
+Movie "1"<-"*" Rental
+Rental "*"<-"1" Customer
+```
 
+### シーケンス図
+statement(計算書生成)メソッドのシーケンス図
+```puml
+@startuml
+-> aCustomer: statement
+   activate aCustomer
+       aCustomer -> aCustomer :*[for all rental]
+       activate aCustomer
+           aCustomer -> aRental :getMovie
+           aCustomer -> aMovie :getPriceCode
+           aCustomer -> aRental :getDaysRented
+       deactivate aCustomer
+   deactivate aCustomer
+@enduml
+```
 ## 実装
+### `Movie.java`
+@import "../../src/main/java/refactoring/videorental/Movie.java"
+### `Rental.java`
+@import "../../src/main/java/refactoring/videorental/Rental.java"
+### `Customer.java`
+@import "../../src/main/java/refactoring/videorental/Customer.java"
--- src/main/java/refactoring/videorental/Customer.java
+++ src/main/java/refactoring/videorental/Customer.java
@@ -1,108 +1,17 @@
-  
-  
-# リファクタリング
-  
-  
-## 要求
-  
-顧客が借りたビデオのレンタル料金を計算して計算書を印刷する。
-
-![](assets/f657651786015ad6423a3b38afc56ad70.png?0.7437116503123407)  
-  
-## 仕様
-  
-+ ビデオレンタルの料金を計算して計算書を印刷するプログラム
-+ システムにはどの映画を何日間借りるかが入力される。
-+ 貸出の日数によって料金が計算され、映画の分類が判定される。
-+ 映画の分類は３つある。一般向け、子供向け、新作。
-+ レンタルポイントも印刷される。新作かどうかによってポイント計算の仕方が異なる。
-  
-## 設計
-  
-### クラス図
-  
-
-![](assets/f657651786015ad6423a3b38afc56ad71.png?0.541518663322422)  
-  
-### シーケンス図
-  
-statement(計算書生成)メソッドのシーケンス図
-
-![](assets/f657651786015ad6423a3b38afc56ad72.png?0.4703129920904352)  
-## 実装
-  
-### `Movie.java`
-  
-```java
 package refactoring.videorental;
-  
-public class Movie {
-  
-    public static final int CHILDRENS = 2;
-    public static final int REGULAR = 0;
-    public static final int NEW_RELEASE = 1;
-  
-    private String _title;
-    private int _priceCode;
-  
-    public Movie(String title, int priceCode) {
-        _title = title;
-        _priceCode = priceCode;
-    }
-  
-    public int getPriceCode() {
-        return _priceCode;
-    }
-  
-    public void setPriceCode(int arg) {
-        _priceCode = arg;
-    }
-  
-    public String getTitle() {
-        return _title;
-    }
-}
-  
-```  
-### `Rental.java`
-  
-```java
-package refactoring.videorental;
-  
-class Rental {
-    private Movie _movie;
-    private int _daysRented;
-  
-    public Rental(Movie movie, int daysRented) {
-        _movie = movie;
-        _daysRented = daysRented;
-    }
-    public int getDaysRented() {
-        return _daysRented;
-    }
-    public Movie getMovie() {
-        return _movie;
-    }
-}
-  
-```  
-### `Customer.java`
-  
-```java
-package refactoring.videorental;
-  
-  
+
+
 import java.util.Enumeration;
 import java.util.Vector;
-  
+
 class Customer {
     private String _name;
     private Vector _rentals = new Vector();
-  
+
     public Customer(String name) {
         _name = name;
     }
-  
+
     public void addRental(Rental arg) {
         _rentals.addElement(arg);
     }
@@ -117,7 +26,7 @@ class Customer {
         while(rentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-  
+
             //一行ごとに金額を計算
             switch (each.getMovie().getPriceCode()) {
                 case Movie.REGULAR:
@@ -133,13 +42,13 @@ class Customer {
                         thisAmount += (each.getDaysRented() - 3) * 1.5;
                     break;
             }
-  
+
             //レンタルポイントを加算
             frequentRenterPoints ++;
             //新作を二日以上借りた場合はボーナスポイント
             if((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
                     each.getDaysRented() > 1) frequentRenterPoints ++;
-  
+
             //この貸し出しに関する数値の表示
             result += "\t" + each.getMovie().getTitle() + "\t" +
                     String.valueOf(thisAmount) + "\n";
@@ -151,6 +60,3 @@ class Customer {
         return result;
     }
 }
-  
-```  
-  
